787dfea1e8fd6fda607d3ada35f179cb36d969e4
Alexandr Boyko <47818976+trpls1x@users.noreply.github.com>
2020-04-22 15:08:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,6 +8,6 @@ self.addEventListener('activate',function(event) {
 });
 
 self.addEventListener('fetch',function(event) {
-    console.log('[Service Worker] Fetching something...');
+    console.log('[Service Worker] Fetching something...',event);
     event.respondWith(fetch(event.request));
 });
